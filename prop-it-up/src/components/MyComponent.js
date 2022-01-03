34b5d8120359cb57fb73189e3de4b86e1806d0e4
wknobbe <wknobbe4@gmail.com>
2022-01-03 14:00:51 -0600

    comment

---
+++ prop-it-up/src/components/MyComponent.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class MyComponent extends Component {
+    render() {
+        return <div>This is a class component.</div>
+    }
+}
+
+export default MyComponent
